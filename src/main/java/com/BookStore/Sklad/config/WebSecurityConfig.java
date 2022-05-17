f553fb26be9794057c042e77db781e29feb4696f
Arti <arkhamb@yandex.ru>
2022-05-17 05:28:41 +0300

    Исправление багов: востановлена работа с post-запросами, скрыта информация, недоступная обычному пользователю

--- src/main/java/com/BookStore/Sklad/config/WebSecurityConfig.java
+++ src/main/java/com/BookStore/Sklad/config/WebSecurityConfig.java
@@ -23,6 +23,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter
     protected void configure(HttpSecurity http) throws Exception
     {
         http
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "/registration").permitAll()
                 .antMatchers("/admin/**","/office/{id}","/book/{id}").hasAuthority("ADMIN")
